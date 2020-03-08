274eddf59039d9e84fa497cee879ce28b54e9654
askhat <sasc@yandex.ru>
2020-03-08 20:36:43 +0300

    refactoring ModelProvider

--- src/main/java/com/github/tinselspoon/intellij/kubernetes/model/ModelProvider.java
+++ src/main/java/com/github/tinselspoon/intellij/kubernetes/model/ModelProvider.java
@@ -24,47 +24,10 @@ public class ModelProvider {
 
     private final ModelLoader modelLoader = new ModelLoader();
 
-    /** The specs currently loaded. */
-    private List<SwaggerSpec> specs;
-
     /** Singleton private constructor. */
     private ModelProvider() {
     }
 
-    /**
-     * Find the model that governs the property described by navigating from the base model of the {@link ResourceTypeKey} through the properties given in the {@code path}.
-     *
-     * @param resourceTypeKey the resource at which to begin the search.
-     * @param path a series of properties to navigate through, may be empty to return the root model of the {@code ResourceTypeKey}.
-     * @return the model, or {@code null} if one cannot be found.
-     */
-    @Nullable
-    public Model findModel(final ResourceTypeKey resourceTypeKey, final List<String> path) {
-        final SwaggerSpec spec = getSpec(resourceTypeKey);
-        final String search = modelIdFromResourceKey(resourceTypeKey);
-        if (spec != null) {
-            Model model = spec.getModels().get(search);
-            for (final String target : path) {
-                if (model != null) {
-                    final Property property = model.getProperties().get(target);
-                    if (property != null) {
-                        if (property.getRef() != null) {
-                            // Look up the ref for the referenced object
-                            model = spec.getModels().get(property.getRef());
-                            continue;
-                        } else if (property.getType() == FieldType.ARRAY && property.getItems() != null && property.getItems().getRef() != null) {
-                            // Look up the ref for the array items
-                            model = spec.getModels().get(property.getItems().getRef());
-                            continue;
-                        }
-                    }
-                    model = null;
-                }
-            }
-            return model;
-        }
-        return null;
-    }
 
     /**
      * Find the properties that may exist as children of the property described by navigating from the base model of the {@link ResourceTypeKey} through the properties given in the {@code path}.
@@ -99,63 +62,95 @@ public class ModelProvider {
      * @return a set of possible kinds.
      */
     public Set<ResourceTypeKey> suggestKinds(@Nullable final String apiVersion) {
-        final Stream<SwaggerSpec> applicableSpecs;
-        if (apiVersion == null) {
-            applicableSpecs = getSpecs().stream();
-        } else {
-            applicableSpecs = getSpecs().stream().filter(s -> apiVersion.equals(s.getApiVersion()));
+        Stream<SwaggerSpec> applicableSpecs = getSpecs().stream();
+        if (apiVersion != null) {
+            applicableSpecs = applicableSpecs.filter(s -> apiVersion.equals(s.getApiVersion()));
         }
 
         // Make a map of kinds to apiVersions - this is not the final data structure we want but is helpful for when we preserve only the most recent API version for a particular kind
         // TODO This does assume that no two API groups will declare the same kind - currently this doesn't happen but the Kubernetes API structure does allow for it
-        Map<String, String> typeKeys = new HashMap<>();
+        Map<String, String> kindToApi = new HashMap<>();
         // Suggest any resource that appears as a return type from an API request
         applicableSpecs.forEach(s -> {
-            final Set<String> apiTypes = new HashSet<>();
-            final Set<String> models = new HashSet<>();
-            for (final Api api : s.getApis()) {
-                for (final ApiOperation operation : api.getOperations()) {
-                    if ("POST".equals(operation.getMethod())) {
-                        apiTypes.add(operation.getType());
-                    }
-                }
+            final Set<String> apiTypes = getApiTypes(s);
 
-            }
-            for (final Model model : s.getModels().values()) {
-                models.add(model.getId());
-            }
+            final Set<String> models = getModels(s);
             // Make sure all API types have an associated model, otherwise there isn't much point in suggesting them as we can't offer anything useful
             apiTypes.retainAll(models);
             for (final String apiType : apiTypes) {
                 String kind = stripModelIdPrefix(apiType);
                 // Only keep the "highest" API version for a kind to ensure we are using the latest version available
-                typeKeys.merge(kind, s.getApiVersion(), (a, b) -> ApiVersionComparator.INSTANCE.compare(a, b) > 0 ? a : b);
+                kindToApi.merge(kind, s.getApiVersion(), (a, b) -> ApiVersionComparator.INSTANCE.compare(a, b) > 0 ? a : b);
             }
         });
 
         // Convert map of type keys to ResourceTypeKey objects
-        return typeKeys.entrySet().stream().map(e -> new ResourceTypeKey(e.getValue(), e.getKey())).collect(Collectors.toSet());
+        return kindToApi.entrySet().stream().map(e -> new ResourceTypeKey(e.getValue(), e.getKey())).collect(Collectors.toSet());
+    }
+
+    @NotNull
+    private Set<String> getModels(SwaggerSpec s) {
+        Set<String> models = new HashSet<>();
+        for (final Model model : s.getModels().values()) {
+            models.add(model.getId());
+        }
+        return models;
+    }
+
+    @NotNull
+    private Set<String> getApiTypes(SwaggerSpec s) {
+        Set<String> apiTypes = new HashSet<>();
+        for (final Api api : s.getApis()) {
+            for (final ApiOperation operation : api.getOperations()) {
+                if ("POST".equals(operation.getMethod())) {
+                    apiTypes.add(operation.getType());
+                }
+            }
+        }
+        return apiTypes;
     }
 
     /**
-     * Gets the child properties of the given {@link Property}. This is done by resolving the property's {@link Property#getRef() ref} to a model and obtaining the properties defined there.
+     * Find the model that governs the property described by navigating from the base model of the {@link ResourceTypeKey} through the properties given in the {@code path}.
      *
-     * @param spec the spec to search for references in.
-     * @param property the property to obtain children for; if {@code null} an empty map is returned.
-     * @return the child properties, or an empty map if no properties could be resolved - e.g. if the property has no ref or the model referenced does not exist.
+     * @param resourceTypeKey the resource at which to begin the search.
+     * @param path a series of properties to navigate through, may be empty to return the root model of the {@code ResourceTypeKey}.
+     * @return the model, or {@code null} if one cannot be found.
      */
-    @NotNull
-    private Map<String, Property> findChildProperties(@NotNull final SwaggerSpec spec, @Nullable final Property property) {
-        if (property != null) {
-            if (property.getRef() != null) {
-                // Look up the ref for the referenced object
-                return getModelProperties(spec, property.getRef());
-            } else if (property.getType() == FieldType.ARRAY && property.getItems() != null && property.getItems().getRef() != null) {
-                // Look up the ref for the array items
-                return getModelProperties(spec, property.getItems().getRef());
+    @Nullable
+    public Model findModel(final ResourceTypeKey resourceTypeKey, final List<String> path) {
+        final SwaggerSpec spec = getSpec(resourceTypeKey);
+        if (spec != null) {
+            return findModel(spec, resourceTypeKey, path);
+        }
+        return null;
+    }
+
+    @Nullable
+    private Model findModel(SwaggerSpec spec, ResourceTypeKey resourceTypeKey, List<String> path) {
+        final String search = modelIdFromResourceKey(resourceTypeKey);
+        Model model = spec.getModels().get(search);
+        for (final String target : path) {
+            if (model != null) {
+                final Property property = model.getProperties().get(target);
+                Model newModel = null;
+                if (property != null) {
+                    if (property.getRef() != null) {
+                        // Look up the ref for the referenced object
+                        newModel = spec.getModels().get(property.getRef());
+                    } else if (isArrayOfModels(property)) {
+                        // Look up the ref for the array items
+                        newModel = spec.getModels().get(property.getItems().getRef());
+                    }
+                }
+                model = newModel;
             }
         }
-        return Collections.emptyMap();
+        return model;
+    }
+
+    private boolean isArrayOfModels(@NotNull Property property) {
+        return property.getType() == FieldType.ARRAY && property.getItems() != null && property.getItems().getRef() != null;
     }
 
     /**
@@ -168,29 +163,11 @@ public class ModelProvider {
      */
     @NotNull
     private Map<String, Property> findProperties(final SwaggerSpec spec, final ResourceTypeKey resourceTypeKey, final List<String> path) {
-        final String search = modelIdFromResourceKey(resourceTypeKey);
-        final Model startingModel = spec.getModels().get(search);
-        if (startingModel != null) {
-            Map<String, Property> properties = startingModel.getProperties();
-            for (final String targetKey : path) {
-                final Property property = properties.get(targetKey);
-                properties = findChildProperties(spec, property);
-            }
-            return properties;
+        final Model model = findModel(spec, resourceTypeKey, path);
+        if (model != null && model.getProperties() != null) {
+            return model.getProperties();
         }
         return Collections.emptyMap();
-    }
-
-    /**
-     * Gets the properties for the model with the specified ID.
-     *
-     * @param spec the spec to search within.
-     * @param modelId the model ID.
-     * @return the model properties, or an empty map if a model with the specified ID cannot be located.
-     */
-    @NotNull
-    private Map<String, Property> getModelProperties(final SwaggerSpec spec, final String modelId) {
-        return spec.getModels().values().stream().filter(m -> modelId.equals(m.getId())).map(Model::getProperties).findAny().orElse(Collections.emptyMap());
     }
 
     /**
